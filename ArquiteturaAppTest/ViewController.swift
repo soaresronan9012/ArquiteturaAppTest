bab66ec543d63161c8db01c7d796d73f5ed2ed03
Ronan Soares <soaresronansilva@gmail.com>
2024-10-27 19:59:02 -0300

    starting

---
+++ ArquiteturaAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArquiteturaAppTest
+//
+//  Created by Ronan Soares on 27/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
